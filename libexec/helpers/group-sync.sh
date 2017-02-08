b454562e06da84c138697aafe9939c46fce9fca8
Álvaro Simón <varosim@gmail.com>
2017-02-08 14:58:52 +0100

    Add singularity group sync

--- libexec/helpers/group-sync.sh
+++ libexec/helpers/group-sync.sh
@@ -6,7 +6,7 @@ singularity_group="singularity"
 
 # Clean singularity group
 clean_singularity_group (){
-    for user in $(getent group "$singularity_group" | cut -d ':' -f 4 | tr ',' ' '); do
+    for user in $(/usr/bin/getent group "$singularity_group" | cut -d ':' -f 4 | tr ',' ' '); do
         # Detect user groups and allow $vsc_group
         if /usr/bin/id -nG "$user"| /usr/bin/grep -qw "$vsc_group"; then
             echo "$user still belongs to $vsc_group group"
@@ -20,6 +20,6 @@ clean_singularity_group (){
 
 clean_singularity_group
 # Add vsc singularity users to group
-for user in $(/usr/bin/getent "$vsc_group"| cut -d ':' -f 4 | tr ',' ' '); do
+for user in $(/usr/bin/getent group "$vsc_group"| cut -d ':' -f 4 | tr ',' ' '); do
     /usr/bin/gpasswd -a "$user" "$singularity_group"
 done
